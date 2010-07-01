aa068ea11c8399c8ddc20f3e6bb3d0da83224f2f
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2010-07-01 14:45:15 +0200

    Work in progress

--- fof.js
+++ fof.js
@@ -1,33 +1,56 @@
 var sys = require('sys'),
-    http = require('http');
+    http = require('http'),
+    arrayDiff = require('./array').array_diff;
 
+if (process.argv.length < 3) {
+    sys.puts('No twitter user name specified.');
+    process.exit(1);
+}
+
+var username = process.argv[2];
 var twitter = http.createClient(80, 'api.twitter.com');
+var friends = [];
+var followers = [];
 
-var getTweets = function(nextCursor) {
+var getTweets = function(nextCursor, twitterApiMethod) {
     var callee = arguments.callee;
-    var friendsRequest = twitter.request('GET', '/1/statuses/friends.json?screen_name=dhofstet&cursor='+nextCursor, {'host': 'api.twitter.com'});
+    var request = twitter.request('GET', '/1/statuses/'+twitterApiMethod+'.json?screen_name='+username+'&cursor='+nextCursor, {'host': 'api.twitter.com'});
 
-    friendsRequest.addListener('response', function(response) {
+    request.addListener('response', function(response) {
 	var data = '';
+
 	response.addListener('data', function(chunk) {
 	    data += chunk;
 	});
+
 	response.addListener('end', function() {
 	    tweets = JSON.parse(data);
 	    var users = tweets['users'], length = users.length;
 
 	    for (var i = 0; i < length; i++) {
+		friends.push(users[i].screen_name);
 		sys.puts(users[i].screen_name);
 	    }
 
 	    sys.puts(tweets.next_cursor);
 
 	    if (tweets.next_cursor > 0) {
-		callee(tweets.next_cursor);
+		callee(tweets.next_cursor, twitterApiMethod);
 	    }
 	});
     });
 
-    friendsRequest.end();
-}(-1);
+    request.end();
+};
 
+getTweets(-1, 'friends');
+getTweets(-1, 'followers');
+
+/*
+sys.puts('--------------------------');
+var diff = arrayDiff(friends, followers);
+
+for (var prop in diff) {
+    sys.puts("\n "+diff[prop]);
+}
+*/
