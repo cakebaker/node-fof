0aafcb634476fb99f28ee4092b52cd874c25e0db
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2011-12-19 14:57:18 +0100

    Replacing util.puts with console.log

--- fof.js
+++ fof.js
@@ -1,5 +1,4 @@
-var util = require('util'),
-    Options = require('./lib/option_parser').Options,
+var Options = require('./lib/option_parser').Options,
     OptionParser = require('./lib/option_parser').OptionParser,
     Twitter = require('./lib/twitter').Twitter,
     Factory = require('./lib/strategies').StrategyFactory;
@@ -15,20 +14,20 @@ switch (selectedOption) {
     case Options.FANS:             return twitter.getUsernames(Factory.getFanStrategy());
     case Options.FRIENDS:          return twitter.getUsernames(Factory.getFriendStrategy());
     case Options.HELP:             return showUsage();
-    case Options.INVALID_OPTION:   util.puts('Invalid option');
-                                   util.puts('');
+    case Options.INVALID_OPTION:   console.log('Invalid option');
+                                   console.log('');
                                    showUsage();
                                    process.exit(1);
 }
 
 function showUsage() {
-    util.puts('Usage: node fof.js (<username>) ([OPTION])');
-    util.puts('List <username>\'s Twitter fans, friends, or people he follows.');
-    util.puts('');
-    util.puts(' --fans         get people who follow <username>, but are not followed back');
-    util.puts(' --following    get people who are followed by <username>, but don\'t follow back');
-    util.puts(' --friends      get people who are followed by <username>, and follow back');
-    util.puts(' --help         display this help and exit');
-    util.puts('');
-    util.puts('If no OPTION is specified, the --following option is used.');
+    console.log('Usage: node fof.js (<username>) ([OPTION])');
+    console.log('List <username>\'s Twitter fans, friends, or people he follows.');
+    console.log('');
+    console.log(' --fans         get people who follow <username>, but are not followed back');
+    console.log(' --following    get people who are followed by <username>, but don\'t follow back');
+    console.log(' --friends      get people who are followed by <username>, and follow back');
+    console.log(' --help         display this help and exit');
+    console.log('');
+    console.log('If no OPTION is specified, the --following option is used.');
 }
